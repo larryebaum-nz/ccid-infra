da08317d03308684681319c2cfff85429389e169
larryebaum <6776768+larryebaum@users.noreply.github.com>
2021-01-08 13:51:12 -0500

    edit vars

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
-variable "aws_region" {
-  type = string
-  default = "eu-west-1"
-}
+# variable "aws_region" {
+#   type = string
+#   default = "eu-west-1"
+# }
 
 variable "owner" {
   description = "name to pass to owner tag"
